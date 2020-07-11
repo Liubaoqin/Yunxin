5c0fe18d644f9c6f1c1d66fea96d60874b89ad16
man <1160643896@qq.com>
2020-07-11 15:37:08 +0800

    first

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Author: LiuBaoqing
+ * Version 1.0
+ * Date: 2020/7/11
+ * Time: 15:32
+ */
+require 'vendor/autoload.php';
+
+use Man\Test;
+
+$Test = new Test();
+$Test->test();
---
+++ src/Test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Author: LiuBaoqing
+ * Version 1.0
+ * Date: 2020/7/11
+ * Time: 15:31
+ */
+
+namespace Man;
+
+
+class Test
+{
+    function test()
+    {
+        echo "Form Test -> test()";
+    }
+}

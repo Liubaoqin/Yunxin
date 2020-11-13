ae7a62f00079e7377eb4629a58e16603a5a5f18a
man <1160643896@qq.com>
2020-11-13 17:18:52 +0800

    设置聊天室内用户角色字符设置

--- src/Api/Room.php
+++ src/Api/Room.php
@@ -124,7 +124,7 @@ class Room extends Base
      * @return mixed
      * @throws Exception
      */
-    public function setMemberRole(string $roomid, string $operator, string $target, string $opt, bool $optvalue, String $notifyExt = '')
+    public function setMemberRole(string $roomid, string $operator, string $target, string $opt, string $optvalue, String $notifyExt = '')
     {
         $data = [
             'roomid' => $roomid,
@@ -389,13 +389,13 @@ class Room extends Base
      * @param string $operator 操作者账号accid
      * @param string $target 被操作者账号accid
      * @param int $opt 1: 设置为管理员，operator必须是创建者 2:设置普通等级用户，operator必须是创建者或管理员 -1:设为黑名单用户，operator必须是创建者或管理员 -2:设为禁言用户，operator必须是创建者或管理员
-     * @param bool $optvalue true或false，true:设置；false:取消设置；执行“取消”设置后，若成员非禁言且非黑名单，则变成游客
+     * @param string $optvalue true或false，true:设置；false:取消设置；执行“取消”设置后，若成员非禁言且非黑名单，则变成游客
      * @param string $notifyExt 通知扩展字段，长度限制2048，请使用json格式
      *
      * @return mixed
      * @throws Exception
      */
-    public function updateMyRoomRole(int $roomid, string $operator, string $target, int $opt, bool $optvalue, string $notifyExt = '')
+    public function updateMyRoomRole(int $roomid, string $operator, string $target, int $opt, string $optvalue, string $notifyExt = '')
     {
         return $this->post('chatroom/setMemberRole.action', [
             'roomid' => $roomid,
